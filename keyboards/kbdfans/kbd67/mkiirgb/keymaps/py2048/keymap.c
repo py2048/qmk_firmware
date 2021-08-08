b8ea197e9bfd2212d8eb68827e392bf0cdeb4e44
py2048 <quannhn2001@gmail.com>
2021-08-08 14:39:12 +0700

    Right shift to end key

--- keyboards/kbdfans/kbd67/mkiirgb/keymaps/py2048/keymap.c
+++ keyboards/kbdfans/kbd67/mkiirgb/keymaps/py2048/keymap.c
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_ESC,           KC_1,      KC_2,      KC_3,      KC_4,      KC_5,      KC_6,      KC_7,      KC_8,      KC_9,      KC_0,           KC_MINS,   KC_EQL,    KC_BSPC,   KC_GRV,
 		KC_TAB,           KC_Q,      KC_W,      KC_E,      KC_R,      KC_T,      KC_Y,      KC_U,      KC_I,      KC_O,      KC_P,           KC_LBRC,   KC_RBRC,   KC_BSLS,   KC_PGUP,
 		LT(_FN1,KC_ESC),  KC_A,      KC_S,      KC_D,      KC_F,      KC_G,      KC_H,      KC_J,      KC_K,      KC_L,      KC_SCLN,        KC_QUOT,              KC_ENT,    KC_PGDN,
-		KC_LSFT,          KC_Z,      KC_X,      KC_C,      KC_V,      KC_B,      KC_N,      KC_M,      KC_COMM,   KC_DOT,    KC_SLSH,        MT(MOD_RSFT, KC_RGHT),KC_UP,     KC_DEL,
+		KC_LSFT,          KC_Z,      KC_X,      KC_C,      KC_V,      KC_B,      KC_N,      KC_M,      KC_COMM,   KC_DOT,    KC_SLSH,        MT(MOD_RSFT, KC_END),KC_UP,     KC_DEL,
 		KC_LCTRL,         KC_LGUI,   KC_LALT,                         KC_SPC,                                     MO(_FN2),  KC_RCTRL,                  KC_LEFT,   KC_DOWN,   KC_RGHT
 		),
 
@@ -65,7 +65,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_ESC,             KC_1,      KC_2,      KC_3,      KC_4,      KC_5,      KC_6,      KC_7,      KC_8,      KC_9,      KC_0,           KC_MINS,   KC_EQL,    KC_BSPC,   KC_GRV,
 		KC_TAB,             KC_Q,      KC_W,      KC_E,      KC_R,      KC_T,      KC_Y,      KC_U,      KC_I,      KC_O,      KC_P,           KC_LBRC,   KC_RBRC,   KC_BSLS,   KC_PGUP,
 		LT(_FNMAC,KC_ESC),  KC_A,      KC_S,      KC_D,      KC_F,      KC_G,      KC_H,      KC_J,      KC_K,      KC_L,      KC_SCLN,        KC_QUOT,              KC_ENT,    KC_PGDN,
-		KC_LSFT,            KC_Z,      KC_X,      KC_C,      KC_V,      KC_B,      KC_N,      KC_M,      KC_COMM,   KC_DOT,    KC_SLSH,        MT(MOD_RSFT, KC_RGHT),KC_UP,     KC_DEL,
+		KC_LSFT,            KC_Z,      KC_X,      KC_C,      KC_V,      KC_B,      KC_N,      KC_M,      KC_COMM,   KC_DOT,    KC_SLSH,        MT(MOD_RSFT, KC_END),KC_UP,     KC_DEL,
 		KC_LCTRL,           KC_LGUI,   KC_LALT,                         KC_SPC,                                     MO(_FN2),  KC_RCTRL,                  KC_LEFT,   KC_DOWN,   KC_RGHT
 		),
 
@@ -130,7 +130,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		_______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   KC_VOLU,
 		_______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,              _______,   KC_VOLD,
 		_______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,              KC_VOLU,   KC_MUTE,
-		_______,   _______,   KC_RALT,                         RGB_TOG,                                    _______,   MO(_FN3),             KC_HOME,   KC_VOLD,   KC_END
+		_______,   _______,   KC_LALT,                         RGB_TOG,                                    _______,   MO(_FN3),             KC_HOME,   KC_VOLD,   KC_END
     ),
 
   /* FN 3 layer
