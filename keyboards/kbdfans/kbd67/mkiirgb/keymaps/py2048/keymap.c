f4c9fb49602aa12d402eff0c8eb7a922b7990858
py2048 <quannhn2001@gmail.com>
2022-03-06 17:16:30 +0700

    Add gaming layer

--- keyboards/kbdfans/kbd67/mkiirgb/keymaps/py2048/keymap.c
+++ keyboards/kbdfans/kbd67/mkiirgb/keymaps/py2048/keymap.c
@@ -20,6 +20,7 @@ enum layers {
     _BASE = 1,
     _MAC = 0,
     _FN1 = 2,
+    _GAME,
     _FNMAC,
     _FNMAC2,
     _FNMACRO,
@@ -69,6 +70,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_RSFT,       LCTL(KC_Z),   LCTL(KC_X),   LCTL(KC_C),   LCTL(KC_V),   LCTL(KC_B),   LCTL(KC_N),   LCTL(KC_M),   LCTL(KC_COMM),   LCTL(KC_DOT),   LCTL(KC_SLSH),   _______,                          KC_HOME,         KC_MUTE,
 		KC_LCTRL,      KC_LGUI,      KC_LALT,                                  KC_SPC,                                                    KC_RALT,        MO(_FN3),                         KC_HOME,         KC_END,          KC_END
         ),
+
+    // Gamming layer
+	[_GAME] = LAYOUT_65_ansi_blocker(
+		KC_ESC,                KC_1,      KC_2,      KC_3,      KC_4,      KC_5,      KC_6,      KC_7,      KC_8,      KC_9,      KC_0,           KC_MINS,   KC_EQL,    KC_BSPC,   KC_GRV,
+		KC_TAB,                KC_Q,      KC_W,      KC_E,      KC_R,      KC_T,      KC_Y,      KC_U,      KC_I,      KC_O,      KC_P,           KC_LBRC,   KC_RBRC,   KC_BSLS,   KC_PGUP,
+		MT(MOD_LCTL ,KC_ESC),  KC_A,      KC_S,      KC_D,      KC_F,      KC_G,      KC_H,      KC_J,      KC_K,      KC_L,      KC_SCLN,        KC_QUOT,              KC_ENT,    KC_PGDN,
+		KC_LSFT,               KC_Z,      KC_X,      KC_C,      KC_V,      KC_B,      KC_N,      KC_M,      KC_COMM,   KC_DOT,    KC_SLSH,        MT(MOD_RSFT, KC_F12), KC_UP,     KC_DEL,
+		KC_LCTRL,              KC_LCTRL,  KC_LALT,                         KC_SPC,                                     KC_RGUI,  MO(_FN2),                   KC_LEFT,   KC_DOWN,   KC_RGHT
+		),
 
 	/* MacOS layer
   * ,--------------------------------------------------------------------------------------------------.
@@ -145,8 +155,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   * `-------------------------------------------------------------------------┘   └-------------+------´
   */
 	[_FN2] = LAYOUT_65_ansi_blocker(
-		KC_ESC,    KC_F1,     KC_F2,     KC_F3,     KC_F4,      KC_F5,    KC_F6,     KC_F7,     KC_F8,     KC_F9,     KC_F10,    DF(_MAC),  DF(_BASE),  _______,    EEP_RST,
-		_______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,    _______,    KC_VOLU,
+		KC_ESC,    KC_F1,     KC_F2,     KC_F3,     KC_F4,      KC_F5,    KC_F6,     KC_F7,     KC_F8,     KC_F9,     KC_F10,    KC_F11,    KC_F12,     _______,    EEP_RST,
+		_______,   DF(_BASE), DF(_MAC),  DF(_GAME), _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,    _______,    KC_VOLU,
 		MO(_FN3),  DM_RSTP,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,               _______,    KC_VOLD,
 		_______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,               C(KC_UP),   KC_MUTE,
 		_______,   _______,   KC_LALT,                         RGB_TOG,                                    MO(_FN3),  _______,             C(KC_LEFT), C(KC_DOWN), C(KC_RGHT)
